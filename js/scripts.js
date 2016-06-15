c878ece833bd270d471de2aa8f4145b549236b35
John <john@epicodus.com>
2016-06-15 16:06:03 -0700

    fix button method

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,47 @@
+var number1;
+var number2;
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var multiply = function(number3, number4) {
-  return number3 * number4;
+var multiply = function(number1, number2) {
+  return number1 * number2;
 };
 
-var subtract = function(number5, number6) {
-  return number5 - number6;
+var subtract = function(number1, number2) {
+  return number1 - number2;
 };
 
-var divide = function(number7, number8) {
-  return number7/number8;
+var divide = function(number1, number2) {
+  return number1/number2;
 };
 
 $(document).ready(function(){
-  $("#add").click(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#num1").val());
-    var number2 = parseInt($("#num2").val());
+
+    $("button").click(function(event) {
+        number1 = parseInt($("#num1").val());
+        number2 = parseInt($("#num2").val());
+        event.preventDefault();
+    })
+
+  $("#add").click(function() {
     var result= add(number1, number2);
     $("#output").text(result);
   });
 
   $("#subtract").click(function() {
-    event.preventDefault();
-    var number3 = parseInt($("#num1").val());
-    var number4 = parseInt($("#num2").val());
-    var result= subtract(number3, number4);
+    var result= subtract(number1, number2);
     $("#output").text(result);
   });
 
   $("#multiply").click(function() {
-    event.preventDefault();
-    var number5 = parseInt($("#num1").val());
-    var number6 = parseInt($("#num2").val());
-    var result= multiply(number5, number6);
+    var result= multiply(number1, number2);
     $("#output").text(result);
   });
 
   $("#divide").click(function() {
-    event.preventDefault();
-    var number7 = parseInt($("#num1").val());
-    var number8 = parseInt($("#num2").val());
-    var result= divide(number7, number8);
+    var result= divide(number1, number2);
     $("#output").text(result);
   });
 
